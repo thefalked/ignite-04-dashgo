4d211d795458a3e78a7c53be71be2547d35fab15
thefalked <giulainocrivelli@hotmail.com>
2021-04-08 01:18:42 -0400

    sidebar now is responsive and created a sidebar drawer

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "@chakra-ui/react";
+import {
+  RiContactsLine,
+  RiDashboard2Line,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="Geral">
+        <NavLink icon={RiDashboard2Line}>Dashboard</NavLink>
+
+        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+      </NavSection>
+      <NavSection title="Automação">
+        <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+
+        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+      </NavSection>
+    </Stack>
+  );
+}
