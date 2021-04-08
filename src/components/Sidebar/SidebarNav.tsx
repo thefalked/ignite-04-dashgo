5bd2017ef88c0cb03b61570c75537c2224fb0b5c
thefalked <giulainocrivelli@hotmail.com>
2021-04-08 01:52:31 -0400

    setting link has active

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -13,14 +13,22 @@ export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="Geral">
-        <NavLink icon={RiDashboard2Line}>Dashboard</NavLink>
+        <NavLink icon={RiDashboard2Line} href="/dashboard">
+          Dashboard
+        </NavLink>
 
-        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        <NavLink icon={RiContactsLine} href="/users">
+          Usuários
+        </NavLink>
       </NavSection>
       <NavSection title="Automação">
-        <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+        <NavLink icon={RiInputMethodLine} href="/forms">
+          Formulários
+        </NavLink>
 
-        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        <NavLink icon={RiGitMergeLine} href="/automation">
+          Automação
+        </NavLink>
       </NavSection>
     </Stack>
   );
